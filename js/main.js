f5a04013962c527ef1eee6f7d8ad7de3612ee1e1
mehmedkadric <kadricze@gmail.com>
2022-03-14 11:04:45 +0100

    Add gitignore and remove scss folder

--- js/main.js
+++ js/main.js
@@ -25,7 +25,6 @@
         }
     });
     
-    
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
@@ -77,4 +76,3 @@
     });
     
 })(jQuery);
-
